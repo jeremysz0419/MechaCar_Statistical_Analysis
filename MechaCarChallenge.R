ffe8b775abd7a979d4d9177ab487da89f7367fc1
jeremysz0419 <jeremiahszhuang@gmail.com>
2022-02-17 22:56:39 -0800

    adding t-test files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,9 @@ head(suspension_coil)
 total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 # lot summary
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+# T-Test on suspension coils
+t.test(suspension_coil$PSI, mu=1500)
+
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
